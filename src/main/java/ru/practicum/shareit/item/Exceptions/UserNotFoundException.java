c0afcaab8355420e4117153641ca97280cfa5d9c
Val2003 <55923298+Val2003@users.noreply.github.com>
2023-07-19 17:43:57 +0400

    Delete src directory

--- src/main/java/ru/practicum/shareit/item/Exceptions/UserNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package ru.practicum.shareit.item.Exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-}
--- src/main/java/ru/practicum/shareit/user/exception/DuplicateEmailException.java
+++
@@ -1,4 +0,0 @@
-package ru.practicum.shareit.user.exception;
-
-public class DuplicateEmailException extends RuntimeException{
-}
