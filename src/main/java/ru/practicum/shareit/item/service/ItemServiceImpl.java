f1fd0a0acdd24e9ed1ad23a557ad8bf887fe7626
Val2003 <val2003@yandex.ru>
2023-10-24 10:19:01 +0400

    убрал maven-enforcer-plugin

---
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -0,0 +1,51 @@
+package ru.practicum.shareit.item.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.storage.ItemStorage;
+import ru.practicum.shareit.mapper.ItemMapper;
+import ru.practicum.shareit.mapper.UserMapper;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class ItemServiceImpl implements ItemService {
+
+    private final ItemStorage itemStorage;
+
+
+    @Override
+    public ItemDto createItem(UserDto userDto, ItemDto itemDto) {
+        return ItemMapper.INSTANCE.toItemDto(itemStorage.createItem(UserMapper.INSTANCE.toUser(userDto), ItemMapper.INSTANCE.toItem(itemDto)));
+    }
+
+    @Override
+    public ItemDto updateItem(Long itemId, ItemDto itemDto, Long userId) {
+        return ItemMapper.INSTANCE.toItemDto(itemStorage.updateItem(itemId, ItemMapper.INSTANCE.toItem(itemDto), userId));
+    }
+
+    @Override
+    public ItemDto getItem(Long itemId, Long userId) {
+        return ItemMapper.INSTANCE.toItemDto(itemStorage.getItem(itemId));
+    }
+
+    @Override
+    public List<ItemDto> getItemsByUser(Long userId) {
+        return itemStorage.getItemsByUser(userId).stream()
+                .map(ItemMapper.INSTANCE::toItemDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ItemDto> getAvailableItems(Long userId, String text) {
+        return itemStorage.getAvailableItems(userId, text).stream()
+                .map(ItemMapper.INSTANCE::toItemDto)
+                .collect(Collectors.toList());
+    }
+}
