cb42a700794bb32e164980e1c2b9eba433e57cfa
Val2003 <val2003@yandex.ru>
2023-10-28 10:57:15 +0400

    правка синтаксиса

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -4,15 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.AnswerBookingDto;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.service.BookingService;
@@ -30,49 +22,49 @@ public class BookingController {
     private final String userIdHeader = "X-Sharer-User-Id";
 
     @PostMapping
-    public ResponseEntity <AnswerBookingDto> createBooking(
+    public ResponseEntity<AnswerBookingDto> createBooking(
             @RequestHeader(userIdHeader) Long userId,
             @Valid @RequestBody BookingDto bookingDto) {
         log.info("POST /bookings : create booking from DTO - {}", bookingDto);
-        return ResponseEntity  .status(HttpStatus.CREATED)
-                .body( bookingService.createBooking(userId, bookingDto));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(bookingService.createBooking(userId, bookingDto));
 
     }
 
     @PatchMapping("/{bookingId}")
-    public ResponseEntity <AnswerBookingDto> confirmationBooking(
+    public ResponseEntity<AnswerBookingDto> confirmationBooking(
             @RequestHeader(userIdHeader) Long userId,
             @PathVariable Long bookingId,
             @RequestParam Boolean approved) {
         log.info("PATCH /bookings/{} : update booking's status by ID - approved {}, user ID {}",
                 bookingId, approved, userId);
-        return ResponseEntity   .status(HttpStatus.OK)
+        return ResponseEntity.status(HttpStatus.OK)
                 .body(bookingService.confirmationBooking(userId, bookingId, approved));
     }
 
     @GetMapping("/{bookingId}")
-    public ResponseEntity <AnswerBookingDto> getBooking(@RequestHeader(userIdHeader) Long userId,
-                                                        @PathVariable Long bookingId) {
+    public ResponseEntity<AnswerBookingDto> getBooking(@RequestHeader(userIdHeader) Long userId,
+                                                       @PathVariable Long bookingId) {
         log.info("GET /bookings/{} : get booking by ID by user ID {}", bookingId, userId);
-        return ResponseEntity   .status(HttpStatus.OK)
+        return ResponseEntity.status(HttpStatus.OK)
                 .body(bookingService.getBooking(userId, bookingId));
     }
 
     @GetMapping
-    public ResponseEntity <List<AnswerBookingDto>> getAllBookingByUser(
+    public ResponseEntity<List<AnswerBookingDto>> getAllBookingByUser(
             @RequestHeader(userIdHeader) Long userId,
             @RequestParam(defaultValue = "ALL", required = false) String state) {
         log.info("GET /bookings?state={} : get list of bookings by user ID {} with state", state, userId);
-        return ResponseEntity   .status(HttpStatus.OK)
+        return ResponseEntity.status(HttpStatus.OK)
                 .body(bookingService.getAllBookingByUser(userId, state));
     }
 
     @GetMapping("/owner")
-    public ResponseEntity <List<AnswerBookingDto>> getAllBookingByOwner(
+    public ResponseEntity<List<AnswerBookingDto>> getAllBookingByOwner(
             @RequestHeader(userIdHeader) Long userId,
             @RequestParam(defaultValue = "ALL", required = false) String state) {
         log.info("GET /bookings/owner?state={} : get list of bookings by owner ID {} with state", state, userId);
-        return ResponseEntity   .status(HttpStatus.OK)
+        return ResponseEntity.status(HttpStatus.OK)
                 .body(bookingService.getAllBookingByOwner(userId, state));
     }
 
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -4,16 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
-
 import ru.practicum.shareit.user.service.UserService;
 
 import javax.validation.Valid;
@@ -31,29 +23,29 @@ public class UserController {
     public ResponseEntity createUser(@RequestBody @Valid UserDto userDto) {
         log.info("POST /users : create user from DTO - {}", userDto);
         return ResponseEntity
-                .status(HttpStatus.OK) .body(userService.createUser(userDto));
+                .status(HttpStatus.OK).body(userService.createUser(userDto));
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity updateUser(@PathVariable("id") Long id,
-                              @RequestBody UserDto userDto) {
+                                     @RequestBody UserDto userDto) {
         log.info("PATCH /users/{} : update user by ID from DTO - {}", id, userDto);
         return ResponseEntity
-                .status(HttpStatus.OK) .body(userService.updateUser(userDto, id));
+                .status(HttpStatus.OK).body(userService.updateUser(userDto, id));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity <UserDto> getUser(@PathVariable("id") Long id) {
+    public ResponseEntity<UserDto> getUser(@PathVariable("id") Long id) {
         log.info("GET /users/{} : get user by ID", id);
         return ResponseEntity
-                .status(HttpStatus.OK) .body(userService.getUser(id));
+                .status(HttpStatus.OK).body(userService.getUser(id));
     }
 
     @GetMapping
-    public ResponseEntity <List<UserDto>> getAllUsers() {
+    public ResponseEntity<List<UserDto>> getAllUsers() {
         log.info("GET /users : get list of all users");
         return ResponseEntity
-                .status(HttpStatus.OK) .body(userService.getAllUsers());
+                .status(HttpStatus.OK).body(userService.getAllUsers());
     }
 
     @DeleteMapping("/{id}")
