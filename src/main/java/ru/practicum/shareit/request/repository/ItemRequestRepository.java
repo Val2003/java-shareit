5b4827da4afab4cb8e7f3acbeda487eb853e2614
Val2003 <val2003@yandex.ru>
2023-11-05 11:39:29 +0400

    правка по замечаниям

--- src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.request.model.ItemRequest;
 
 import java.util.List;
+
 @Repository
 public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
 
