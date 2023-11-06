fc9c275f720fe2dd9507085b5d8cedf43221e0b2
Val2003 <val2003@yandex.ru>
2023-11-06 19:45:22 +0400

    основные изменения в ДЗ 16

---
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.item.dto;
+
+import jdk.jfr.BooleanFlag;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.user.model.User;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ItemDto {
+
+    private Long id;
+
+    @NotBlank(message = "Item's name missing")
+    private String name;
+
+    @NotEmpty(message = "Item's description can't be empty")
+    private String description;
+
+    @BooleanFlag
+    @NotNull(message = "Item's available status can't be null")
+    private Boolean available;
+
+    private User owner;
+
+    private Long requestId;
+
+}
---
+++ server/src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -0,0 +1,35 @@
+package ru.practicum.shareit.request.model;
+
+import lombok.*;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "requests")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class ItemRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "requester_id")
+    private User requester;
+
+    @Column(name = "creation_date")
+    private LocalDateTime created;
+
+}
